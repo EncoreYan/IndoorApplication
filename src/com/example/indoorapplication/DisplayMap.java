d7956cf3c6652bee9e2219ad9d627e9b6252d1ce
Marten <marteffyo@gmail.com>
2013-04-04 19:54:45 +0300

    Showing points on map

--- src/com/example/indoorapplication/DisplayMap.java
+++ src/com/example/indoorapplication/DisplayMap.java
@@ -1,18 +1,71 @@
 package com.example.indoorapplication;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 import android.support.v4.app.NavUtils;
 
 public class DisplayMap extends Activity {
 
+    Canvas c;
+    Paint p;
+    ImageView v;
+    Bitmap bm;
+	ArrayList<Point> points = new ArrayList<Point>();
+	
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(new Map(this));
+        
+        
         setContentView(R.layout.activity_display_map);
         //getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    
+
+    public boolean onTouchEvent(MotionEvent event){
+
+        v = (ImageView)findViewById(R.id.mapView);
+	    bm = Bitmap.createBitmap(v.getWidth(),v.getHeight(),Config.ARGB_8888);
+	    c = new Canvas(bm);
+	    p = new Paint();        
+	    p.setColor(Color.BLUE);
+	    
+	    int[] coords = new int[2];
+	    v.getLocationOnScreen(coords);
+	    
+	    float x = event.getRawX()-coords[0]; 
+	    float y = event.getRawY()-coords[1];
+	    Point currentPoint = new Point(x, y, "BLUE");
+	    points.add(currentPoint);
+	    
+	    for(int i = 0; i < points.size(); i++){
+	    	if(points.get(i).getColor().equals("BLUE")){
+	    		p.setColor(Color.BLUE);
+	    	} else
+	    		p.setColor(Color.RED);
+	    	
+	    	c.drawCircle(points.get(i).getX(), points.get(i).getY(), 10, p);  
+	    }
+	       
+	
+	    v.setImageBitmap(bm);
+	
+	    return true;
     }
 
     @Override
